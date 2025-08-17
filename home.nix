e6f0d9c443ba4aae12c118899d2463935a66a57c
DXC-0 <62696671+DXC-0@users.noreply.github.com>
2025-08-17 22:19:12 +0000

    Add files via upload

---
+++ home.nix
@@ -0,0 +1,45 @@
+{ pkgs, ... }:
+
+
+{
+
+##### GLOBAL CONFIGURATION #####
+
+  home.username = "alerion";
+  home.homeDirectory = "/home/alerion/";
+  home.stateVersion = "20.09";
+
+  targets.genericLinux.enable = true;
+  xdg.enable = true;
+
+
+  home.packages = with pkgs; [
+    vscode
+    pavucontrol
+    blueberry
+    pinta
+    podman-desktop
+    gnome-boxes
+    eog
+    gnome-text-editor
+    signal-desktop
+    vlc
+    freetube
+  ];
+
+
+  imports = [
+    ./applications/alacritty.nix
+    ./applications/chromium.nix
+    ./applications/dunst.nix
+    ./applications/fastfetch.nix
+    ./applications/firefox.nix
+    ./applications/freetube.nix
+    ./applications/git.nix
+    ./applications/mimeapps.nix
+    ./applications/niri.nix
+    ./applications/tmux.nix
+    ./applications/wofi.nix
+  ];
+
+}
