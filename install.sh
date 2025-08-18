1bba07cad62eb53651de54320ccc682a291fccc7
DXC-0 <62696671+DXC-0@users.noreply.github.com>
2025-08-18 23:24:57 +0000

    Update install.sh

--- install.sh
+++ install.sh
@@ -54,7 +54,7 @@ sudo dnf install --setopt=install_weak_deps=False -y \
   xdg-desktop-portal-gnome \
   xdg-desktop-portal-wlr
 
-echo "📦 Ajoute des applications flatpak 📦"
+echo "📦 Installation des applications flatpak 📦"
 
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak -y install flathub \
