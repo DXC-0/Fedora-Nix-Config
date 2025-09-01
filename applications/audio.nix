a2122f3b819497b71bcea3e403026f5d66e10349
DXC-0 <62696671+DXC-0@users.noreply.github.com>
2025-09-01 09:22:40 +0000

    Update audio.nix

--- applications/audio.nix
+++ applications/audio.nix
@@ -10,5 +10,17 @@
       wpctl set-volume @DEFAULT_AUDIO_SOURCE@ 0.7
     '';
     executable = true;
+    force = true;
+  };
+
+  home.file."default-profile" = {
+    target = ".local/state/wireplumber/default-profile";
+    text = ''
+      [default-profile]
+      alsa_card.pci-0000_01_00.1=off
+      alsa_card.pci-0000_10_00.6=off
+      alsa_card.usb-UGREEN_Camera_UGREEN_Camera_SN0001-02=off
+    '';
+    force = true;
   };
 }
